c0dd8cebfa4204da53933803d57fd1b9d5240a9f
demon-i386 <muranotatsuo@protonmail.com>
2023-05-08 16:10:53 -0300

    Create deploy.js

---
+++ SmartContractDeployment/scripts/deploy.js
@@ -0,0 +1,13 @@
+async function main() {
+   const HelloWorld = await ethers.getContractFactory("OfCfuWGxJKWcGlrmLKot");
+   // Start deployment, returning a promise that resolves to a contract object
+   const hello_world = await HelloWorld.deploy();
+   console.log("Contract deployed to address:", hello_world.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+});
