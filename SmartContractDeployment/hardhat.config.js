76fa934fe3b9ecaa57865def327ee90828b2f7b1
demon-i386 <muranotatsuo@protonmail.com>
2023-05-08 16:09:30 -0300

    Create hardhat.config.js

---
+++ SmartContractDeployment/hardhat.config.js
@@ -0,0 +1,15 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+module.exports = {
+   solidity: "0.8.17",
+   defaultNetwork: "polygon_mumbai",
+   networks: {
+      hardhat: {},
+      polygon_mumbai: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
